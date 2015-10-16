1f409d41c2fb78b047ec0d80374179454794ac29
Cory Larcade <cory.larcade@familysearch.org>
2015-10-16 11:02:32 -0600

    Ripping out AngularJS and preparing for newSite3 tech.

--- directives/publicWrapper.js
+++
@@ -1,9 +0,0 @@
-app.directive('publicWrapper',
-   function () {
-      return {
-         restrict: 'E',
-         transclude: true,
-         templateUrl: 'templates/publicwrapper.html'
-      };
-   }
-);
