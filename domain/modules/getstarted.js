6fa2efbf2a570b5ad9df6ad3563f15963f4b6e3c
Cory Larcade <cory.larcade@familysearch.org>
2015-12-22 20:40:59 -0700

    Refactored getstarted and personal views to ejs files.

--- domain/modules/getstarted.js
+++ domain/modules/getstarted.js
@@ -1,18 +1,16 @@
 var express = require('express');
-var path = require('path');
 
-var REGISTER_ROUTER = (function(path) {
+var REGISTER_ROUTER = (function() {
   var register_router = express.Router({mergeParams: true});
-  var base_url = __dirname + '/../../public/views/';
 
   register_router.route('/')
     .get(function (req, res) {
-      res.sendFile(path.resolve(base_url + 'getstarted.html'));
+      res.render('getstarted');
     });
 
   register_router.route('/personal')
     .get(function (req, res) {
-      res.sendFile(path.resolve(base_url + 'personal.html'));
+      res.render('personal');
     })
     .post(function (req, res) {
     });
@@ -23,6 +21,6 @@ var REGISTER_ROUTER = (function(path) {
     });
 
   return register_router;
-})(path);
+})();
 
 module.exports = REGISTER_ROUTER;
