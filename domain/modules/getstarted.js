d27f64317695f22e77bb1209bf66234ada9dc16b
Cory Larcade <cory.larcade@familysearch.org>
2015-12-23 00:26:02 -0700

    Added client-side form validation on personal.ejs view, and removed unused code.

--- domain/modules/getstarted.js
+++ domain/modules/getstarted.js
@@ -13,12 +13,48 @@ var REGISTER_ROUTER = (function() {
       res.render('personal');
     })
     .post(function (req, res) {
+      if (!req.body) {
+        console.log("Error: request missing body.");
+        res.status(500).send("Couldn't create new account/user.");
+      } else {
+        var response_data = req.body;
+
+        if (!response_data.firstName) {
+          res.status(500).send('Item missing first name.');
+        } else if (!response_data.lastName) {
+          res.status(500).send('Item missing last name.');
+        } else if (!response_data.email) {
+          res.status(500).send('Item missing email.');
+        } else if (!response_data.password) {
+          res.status(500).send('Item missing password.');
+        } else {
+          try {
+            var first_name = response_data.firstName;
+            var last_name = response_data.lastName;
+            var email = response_data.email;
+            var password = response_data.password;
+
+            // TODO: Sanitize user input
+
+            // TODO: Insert data into database
+
+          } catch (err) {
+            console.log("Error: ", err);
+            res.status(500).send('Failed to register new user/account');
+          }
+        }
+      }
     });
 
   register_router.route('/business')
     .get(function (req, res) {
       res.render('business');
     });
+
+  function validateName(name) {
+
+    return true;
+  }
 
   return register_router;
 })();
--- server.js
+++ server.js
@@ -27,19 +27,7 @@ function startServer() {
 
   //var item_router = require('./domain/modules/item');
   //app.use('/user/:user_id/item', item_router);
-  //
-  //var budget_router = require('./domain/modules/budget');
-  //app.use('/user/:user_id/budget', budget_router);
-  //
-  //var items_router = require('./domain/modules/items');
-  //app.use('/user/:user_id/items', items_router);
-  //
-  //var budgets_router = require('./domain/modules/budgets');
-  //app.use('/user/:user_id/budgets', budgets_router);
 
-  //app.listen(3000, function () {
-  //  console.log("started server");
-  //});
   app.listen(app.get('port'), function () {
     console.log("Node app is running on port", app.get('port'));
   });
