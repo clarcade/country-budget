8c4d6e17568a20bfc2a604ad31ff36c9c6486874
Cory Larcade <cory.larcade@familysearch.org>
2016-01-18 15:02:29 -0700

    Made the Header nav more rebust and made it apparent which header li is selected. Navigation still has bugs.

--- public/assets/js/lihp.js
+++ public/assets/js/lihp.js
@@ -1,7 +1,11 @@
 var MAIN = (function (main) {
   main.init = function () {
+    if (!main.HEADER) console.log("main.HEADER missing");
     main.HEADER.init();
+    if (!main.ITEM) console.log("main.ITEM missing");
     main.ITEM.init();
+    if (!main.REPORT) console.log("main.REPORT missing");
+    main.REPORT.init();
   };
 
   return main;
